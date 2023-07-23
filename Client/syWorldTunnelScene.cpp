9644520335765e1a0fb74757e6019e8119caff93
승연 정 <wjdtmd486@naver.com>
2023-07-23 18:03:53 +0900

    WorldTunnelScene 적 오브젝트 삭제

--- Client/syWorldTunnelScene.cpp
+++ Client/syWorldTunnelScene.cpp
@@ -12,14 +12,6 @@
 #include "syPlayer.h"
 #include "syCollisionManager.h"
 
-#include "syWaddleDee.h"
-#include "syBlockEnemy.h"
-#include "syFlower.h"
-#include "syHotHead.h"
-#include "syHotHead_Fire.h"
-#include "syIce.h"
-#include "sySirKibble.h"
-
 
 namespace sy
 {
@@ -37,25 +29,6 @@ namespace sy
 		Vector2 vec = Vector2(Application::GetResolution()) / 2.f;
 		vec.y /= 2.f;
 		Bg->GetComponent<Transform>()->SetPosition(vec); // 중점 설정
-
-		// 적 생성
-		WaddleDee* waddleDee = object::Instantiate<WaddleDee>(eLayerType::Enemy);
-		waddleDee->GetComponent<Transform>()->SetPosition(Vector2(100.f, 100.f));
-
-		BlockEnemy* Block = object::Instantiate<BlockEnemy>(eLayerType::Enemy);
-		Block->GetComponent<Transform>()->SetPosition(Vector2(150.f, 100.f));
-
-		Flower* flower = object::Instantiate<Flower>(eLayerType::Enemy);
-		flower->GetComponent<Transform>()->SetPosition(Vector2(200.f, 100.f));
-
-		HotHead* hotHead = object::Instantiate<HotHead>(eLayerType::Enemy);
-		hotHead->GetComponent<Transform>()->SetPosition(Vector2(250.f, 100.f));
-
-		Ice* ice = object::Instantiate<Ice>(eLayerType::Enemy);
-		ice->GetComponent<Transform>()->SetPosition(Vector2(300.f, 100.f));
-
-		SirKibble* sirkibble = object::Instantiate<SirKibble>(eLayerType::Enemy);
-		sirkibble->GetComponent<Transform>()->SetPosition(Vector2(350.f, 100.f));
 
 		Scene::Initialize();
 	}
