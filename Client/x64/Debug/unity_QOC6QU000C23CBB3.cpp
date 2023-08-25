633b7be60ea068415703a2cc734ff73aff8bf6d7
승연 정 <wjdtmd486@naver.com>
2023-08-25 22:29:09 +0900

    .gitignore 수정

---
+++ Client/x64/Debug/unity_QOC6QU000C23CBB3.cpp
@@ -0,0 +1,378 @@
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syAbilityTestScene.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syBossEnemy.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syCutterKirby_Skill.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syAbilityItem.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syAbilityStar.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syBreath_Effect.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syCollider.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syCollisionManager.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syCushyCloudScene.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syCutterKirby.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syDanceScene.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syDarkNebula.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syDarkNebula_Border.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syDarkNebula_DeadStar.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syDarkNebula_Eye.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syDarkNebula_Fire.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syDarkNebula_FireBall.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syDarkNebula_IceSkill.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syDarkNebula_SparkBolt.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syDarkNebula_SparkSkill.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syDarkNebula_SparkSkill_Collider.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syDarkNebula_Star.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syDaroach.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syDaroach_Bomb.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syDaroach_Charge_Energe.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syDaroach_Energe.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syDaroach_Star.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syDaroach_TimeBomb.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syDash_Effect.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syDotUI.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syCrimp_Skill.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syEnemyHPbarUI.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syFireKirby.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syFireKirby_Skill.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syGambleGalaxyScene.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syHotHead_Fire.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syAbilityUI.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syAnimation.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syAnimator.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syBackGround.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syBlockin.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syCamera.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syComponent.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syEffects.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syEndingScene.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syEnemy.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syEntity.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\Main.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syForeGround.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syHotHead.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syHPbarUI.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syIce_Enemy.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syIceIslandScene.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syIceKirby.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syIceKirby_Skill.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syInhale_Effect.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syIntroScene.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syInventory.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syInventoryItem.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syJamJungleScene.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syKingDedede.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syKirby.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syLanding_Effect.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syLevel1_BossScene.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syLevel6_BossScene.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syLevel7_BossScene.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syLevel8_BossScene.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syLevel_BG.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syLifeUI.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syMetaKnight.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syMixItem.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syNatureNotchScene.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syNinjaKirby.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syNinjaKirby_ChargeEffect.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syNinjaKirby_Fire.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syNinjaKirby_Shuriken.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syNormal_Skill.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syNumberUI.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syPlacardUI.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syPlayer.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syPortalUI.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syPrismPlainsScene.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syRigidbody.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\sySecretSeaScene.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\sySirKibble_Skill.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\sySound.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\sySoundManager.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\sySparkKirby.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\sySparkKirby_Skill.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syLevel1_Stage2Scene.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syLevel1_Stage3Scene.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syLevel1_Stage4Scene.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syStar_Effect.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syStepUI.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syStarUI.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syTornadoKirby.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syTornado_Effect.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syTransformEffect.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syVocalVolcanoScene.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syWaddleDee.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\sySirKibble.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syTexture.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syLevelSelectScene.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syMath.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syOpeningScene.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syDefaultKirby.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syResource.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syResourceManager.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syScene.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syApplication.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syGameObject.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syInput.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syLayer.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\sySceneManager.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\sySpriteRenderer.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syLevel1_Stage1Scene.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syTime.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syTitleScene.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syTransform.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syUI.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syVideo.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syTunnel_BG.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syTunnelScene.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syCrimp.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syZoom_Effect.cpp"
+
+
+#include "C:\Users\JSY\Desktop\Kirby_Squeak_Squad\Client\syWheelKirby.cpp"
+
