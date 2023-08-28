f9d887d3b05070d26289d41d57c643763a90b8cd
승연 정 <wjdtmd486@naver.com>
2023-08-28 12:44:23 +0900

    MetaKnight_SlashSkill 로직 구현

--- Client/syMetaKnight_SlashSkill.cpp
+++ Client/syMetaKnight_SlashSkill.cpp
@@ -4,13 +4,41 @@
 #include "syEnemy.h"
 #include "syMetaKnight.h"
 #include "syPlayer.h"
-
+#include "syTexture.h"
+#include "syResourceManager.h"
+#include "syAnimator.h"
+#include "syTime.h"
 
 namespace sy
 {
-	MetaKnight_SlashSkill::MetaKnight_SlashSkill(MetaKnight* owner)
+	MetaKnight_SlashSkill::MetaKnight_SlashSkill(MetaKnight* owner, Vector2 MoveDir)
 		: Effects(owner)
+		, mTransform(nullptr)
+		, mMoveDir(MoveDir)
+		, mDuration(0.f)
 	{
+		mTransform = GetComponent<Transform>();
+		mTransform->SetPosition(GetOwner()->GetComponent<Transform>()->GetPosition());
+		GetComponent<Collider>()->SetSize(Vector2(42.f, 73.5f));
+
+		// 텍스쳐 로드
+		Texture* MetaKnight_SlashSkill_Right_Tex = ResourceManager::Load<Texture>(L"MetaKnight_SlashSkill_Right_Tex", L"..\\Resources\\Enemy\\Boss\\MetaKnight\\MetaKnight_SlashSkill_Right.bmp");
+		Texture* MetaKnight_SlashSkill_Left_Tex = ResourceManager::Load<Texture>(L"MetaKnight_SlashSkill_Left_Tex", L"..\\Resources\\Enemy\\Boss\\MetaKnight\\MetaKnight_SlashSkill_Left.bmp");
+
+		MetaKnight_SlashSkill_Right_Tex->SetScale(Vector2(0.35f, 0.35f));
+		MetaKnight_SlashSkill_Left_Tex->SetScale(Vector2(0.35f, 0.35f));
+
+		Animator* animator = GetComponent<Animator>();
+
+		// 애니메이션 생성
+		Vector2 Animationoffset = Vector2(0.f, 0.f);
+		animator->CreateAnimation(MetaKnight_SlashSkill_Right_Tex, L"MetaKnight_SlashSkill_Right", Vector2::Zero, Vector2(120.f, 210.f), Vector2(120.f, 0.f), 0.1f, 4, Animationoffset);
+		animator->CreateAnimation(MetaKnight_SlashSkill_Left_Tex, L"MetaKnight_SlashSkill_Left", Vector2::Zero, Vector2(120.f, 210.f), Vector2(120.f, 0.f), 0.1f, 4, Animationoffset);
+
+		if (owner->GetComponent<Transform>()->GetDirection() == eDirection::RIGHT)
+			animator->PlayAnimation(L"MetaKnight_SlashSkill_Right", true);
+		else
+			animator->PlayAnimation(L"MetaKnight_SlashSkill_Left", true);
 	}
 
 	MetaKnight_SlashSkill::~MetaKnight_SlashSkill()
@@ -24,6 +52,18 @@ namespace sy
 
 	void MetaKnight_SlashSkill::Update()
 	{
+		mDuration += Time::DeltaTime();
+
+		if (mDuration > 5.f)
+		{
+			Destroy(this);
+		}
+
+		Vector2 pos = mTransform->GetPosition();
+		mMoveDir.Normalize();
+		pos += mMoveDir * 200.f * Time::DeltaTime();
+		mTransform->SetPosition(pos);
+		
 		Effects::Update();
 	}
 
--- Client/syMetaKnight_SlashSkill.h
+++ Client/syMetaKnight_SlashSkill.h
@@ -3,12 +3,13 @@
 
 namespace sy
 {
+    using namespace math;
     class MetaKnight;
     class MetaKnight_SlashSkill : public Effects
     {
         friend class MetaKnight;
     private:
-        MetaKnight_SlashSkill(MetaKnight* owner);
+        MetaKnight_SlashSkill(MetaKnight* owner, Vector2 MoveDir);
         virtual ~MetaKnight_SlashSkill();
 
         virtual void Initialize();
@@ -17,6 +18,11 @@ namespace sy
 
         virtual void OnCollisionEnter(class Collider* other);
         virtual void OnCollisionStay(class Collider* other);
+
+    private:
+        class Transform*        mTransform;
+        Vector2                 mMoveDir;
+        float                   mDuration;
     };
 }
 
